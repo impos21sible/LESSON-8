cfcc0578f3885352c04a756adfdba9e907da79e9
Aleksandr <116497492+impos21sible@users.noreply.github.com>
2024-11-12 19:33:10 +0300

    исправил TransactionValidator.java

--- untitled/src/TransactionValidator.java
+++ untitled/src/TransactionValidator.java
@@ -1,20 +1,15 @@
 public class TransactionValidator {
+    public static final double MIN_AMOUNT = 1.0;
+    public static final double MAX_AMOUNT = 5000.0;
 
-    public static boolean isValidAmount(Double amount)
-    {
-        final Double MIN_AMOUNT = 1.0;
-        final Double MAX_AMOUNT  = 5000.0;
-
-        if (amount < MIN_AMOUNT || amount > MAX_AMOUNT)
-        {
+    public static boolean isValidAmount(Double amount) {
+        if (amount < MIN_AMOUNT) {
             System.out.println("Минимальная сумма перевода: " + MIN_AMOUNT + " р. Попробуйте ещё раз!");
+            return false;
+        } else if (amount > MAX_AMOUNT) {
             System.out.println("Максимальная сумма перевода: " + MAX_AMOUNT + " р. Попробуйте ещё раз!");
-            return  false;
-
+            return false;
         }
-
-
-
         return true;
     }
 }
