be554606ef4e6d91998d78d9d93ad85ae52a3c20
Aleksandr <116497492+impos21sible@users.noreply.github.com>
2024-11-12 19:50:43 +0300

    Задание2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,16 +1,12 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Пожалуйста, введите сумму перевода в рублях.");
         Double amount = scanner.nextDouble();
-
         boolean isValid = TransactionValidator.isValidAmount(amount);
-                if(isValid){
-                    System.out.println("Спасибо! Ваш перевод на сумму " + amount + " р. успешно выполнен.");
-
-                }
+        if(isValid){
+            System.out.println("Спасибо! Ваш перевод на сумму " + amount + " р. успешно выполнен.");
+        }
     }
 }
